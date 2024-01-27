90e8db585c98afaf16de61f6bd81a345996d0e64
VUVANDUC2 <vanduc6122003@gmail.com>
2024-01-27 07:38:04 +0700

    Demo

---
+++ BTTuan3/BTTuan3/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTTuan3
+{
+    internal class Demo
+    {
+        int a;
+        int b;
+        int c;
+    }
+}
--- BTTuan3/BTTuan3/obj/Debug/net8.0/BTTuan3.AssemblyInfo.cs
+++ BTTuan3/BTTuan3/obj/Debug/net8.0/BTTuan3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTTuan3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438135089f884cc52941f4a3b1b52a11dd09290a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTTuan3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTTuan3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
